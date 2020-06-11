1bb94ad3d747f7a8e637d1dc827d987c6239d0ad
Nigel Banks <nigel.g.banks@gmail.com>
2020-06-11 14:56:38 +0100

    Redirect logs to stdout/stderr, and some formatting.

--- fcrepo/rootfs/etc/cont-init.d/03-fcrepo-setup.sh
+++ fcrepo/rootfs/etc/cont-init.d/03-fcrepo-setup.sh
@@ -3,3 +3,7 @@ set -e
 # Key needs to be present before startup otherwise,
 # it will ignore subsequent requests even if the key has been generated.
 timeout 300 bash -c 'until [[ -f /opt/keys/claw/public.key ]]; do sleep 1; done'
+
+# Change log files to redirect to stdout/stderr
+ln -sf /dev/stdout /opt/tomcat/logs/velocity.log
+chown tomcat:tomcat /opt/tomcat/logs/velocity.log
--- nginx/rootfs/etc/cont-init.d/02-ngnix-install.sh
+++ nginx/rootfs/etc/cont-init.d/02-ngnix-install.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
 mkdir /run/nginx &> /dev/null || true
+
+# Change log files to redirect to stdout/stderr
+ln -sf /dev/stdout /var/log/nginx/access.log
+chown nginx:nginx /var/log/nginx/access.log
+
+ln -sf /dev/stderr /var/log/nginx/error.log
+chown nginx:nginx /var/log/nginx/error.log
