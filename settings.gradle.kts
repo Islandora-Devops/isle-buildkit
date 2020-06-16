08b2baec712a42ace29cebc750f5d229f6cc3eef
Nigel Banks <nigelgbanks@users.noreply.github.com>
2020-06-16 15:30:06 +0100

    Use plugin for build logic. (#51)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
-rootProject.name = "docker"
+rootProject.name = "isle-buildkit"
+
+// Include plugin for building Docker images.
+sourceControl {
+    gitRepository(uri("https://github.com/Islandora-Devops/isle-gradle-docker-plugin.git")) {
+        producesModule("ca.islandora:isle-gradle-docker-plugin")
+    }
+}
 
 // Include any folder that has a Dockerfile as a sub-project.
 rootProject.projectDir
