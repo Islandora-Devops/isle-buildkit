7babb6790c65370747deae0251bbbe4946b0cfbe
dannylamb <dlamb@islandora.ca>
2020-07-08 14:22:55 +0000

    TLS

--- rename.sh
+++ rename.sh
@@ -4,6 +4,6 @@ for full_path in **/rootfs/etc/cont-init.d/*.sh; do
   name=${full_filename#*-}
   number=${full_filename%-$name}
   single=${number#0}
-  new_number=$(($single + 1))
+  new_number=$(($single - 1))
   mv "$full_path" "$path/0$new_number-$name" 
 done
