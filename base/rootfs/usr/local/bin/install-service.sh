22067c19516ace1e999a4c7dd43b74118c9cfc5a
Nigel Banks <nigel.g.banks@gmail.com>
2021-01-13 22:51:28 +0000

    Made logic for creating a service user generic so it could be used by things other than apache tools.

--- base/rootfs/usr/local/bin/install-service.sh
+++ base/rootfs/usr/local/bin/install-service.sh
@@ -7,25 +7,26 @@ readonly ARGS="$@"
 function usage() {
     cat <<- EOF
     usage: $PROGNAME options [FILE]...
- 
-    Installs the given apache service in /opt. Creates a user/group for the
-    service and ensuring that all files are owned by that user/group.
+
+    Creates a user/group for the service and as well as a directory in /opt
+    ensuring that all files are owned by that user/group.
+
+    Unpacks the specified archive into the install directory as well.
 
     Additional parameters are files to be removed from the installation to save
-    on space. Things like "examples", and "docs".
- 
+    on space. Things like "examples", and "docs", etc.
+
     OPTIONS:
        -n --name          The name of the services to install (used to create user/group and install directory).
-       -k --key           GPG Key used to verify the downloaded file.
-       -f --file          The name of the file to download.
+       -f --file          The name of the archive to unpack into the install directory.
+       -d --depth         Some archives have extraneous parent folders they are nested in the depth indicates how many should be ignored, defaults to 1.
        -h --help          Show this help.
        -x --debug         Debug this script.
- 
+
     Examples:
        Install ActiveMQ:
        $PROGNAME \\
                  --name "activemq" \\
-                 --key "62ED4DF0BACB8793" \\
                  --file "apache-activemq-5.14.5-bin.tar.gz" \\
                  examples webapps-demo docs
 EOF
@@ -39,8 +40,8 @@ function cmdline() {
         case "$arg" in
             # Translate --gnu-long-options to -g (short options)
             --name)       args="${args}-n ";;
-            --key)        args="${args}-k ";;
             --file)       args="${args}-f ";;
+            --depth)      args="${args}-d ";;
             --help)       args="${args}-h ";;
             --debug)      args="${args}-x ";;
             # Pass through anything else
@@ -48,21 +49,21 @@ function cmdline() {
                args="${args}${delim}${arg}${delim} ";;
         esac
     done
- 
+
     # Reset the positional parameters to the short options
     eval set -- $args
- 
-    while getopts "n:k:f:hx" OPTION
+
+    while getopts "n:f:d:hx" OPTION
     do
         case $OPTION in
         n)
             readonly NAME=${OPTARG}
             ;;
-        k)
-            readonly KEY=${OPTARG}
-            ;;
         f)
             readonly FILE="${OPTARG}"
+            ;;
+        d)
+            readonly DEPTH="${OPTARG}"
             ;;
         h)
             usage
@@ -75,9 +76,14 @@ function cmdline() {
         esac
     done
 
-    if [[ -z $NAME || -z $KEY || -z $FILE ]]; then
-        echo "Missing one or more required options: --name --key --file"
+    if [[ -z $NAME || -z $FILE ]]; then
+        echo "Missing one or more required options: --name --file"
         exit 1
+    fi
+
+    # Use default depth if not specified.
+    if [[ -z $DEPTH ]]; then
+        readonly DEPTH=1
     fi
 
     # All remaning parameters are files to be removed from the installation.
@@ -90,10 +96,19 @@ function cmdline() {
 function main {
     cmdline ${ARGS}
     local install_directory=/opt/${NAME}
-    local user=${NAME}
-    local group=${NAME}
-    gpg --keyserver hkp://pool.sks-keyservers.net --recv-key ${KEY}
-    gpg --verify ${FILE}.asc ${FILE}
-    install-service.sh --name "${NAME}" --file "${FILE}" ${REMOVE[@]}
+    create-service-user.sh --name ${NAME}
+    case $FILE in 
+    *.tar.gz|*.tgz)
+        s6-setuidgid ${NAME} tar -xzf ${FILE} -C ${install_directory} --strip-components ${DEPTH}
+        ;;
+    *)
+        echo "Unable to unpack ${FILE} please update script to support additional formats."
+        exit 1
+        ;;
+    esac
+    # Remove extraneous files.
+    for i in "${REMOVE[@]}"; do
+        rm -fr "${install_directory}/${i}"
+    done
 }
 main
