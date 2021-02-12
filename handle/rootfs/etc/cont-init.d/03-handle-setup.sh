c92796d49d68a35485c7f07c1a7be9618a1cbf9c
Nigel Banks <nigel.g.banks@gmail.com>
2021-02-12 18:22:15 +0000

    Changed to follow new conventions around Confd/Environment variables.

--- handle/rootfs/etc/cont-init.d/03-handle-setup.sh
+++ handle/rootfs/etc/cont-init.d/03-handle-setup.sh
@@ -2,6 +2,15 @@
 set -e
 
 function execute_sql_file {
+    local HANDLE_DB_HOST=""
+    local HANDLE_DB_PORT=""
+    if [[ "${HANDLE_PERSISTENCE_TYPE}" == "mysql" ]]; then
+        HANDLE_DB_HOST="${HANDLE_DB_MYSQL_HOST}"
+        HANDLE_DB_PORT="${HANDLE_DB_MYSQL_PORT}"
+    else
+        HANDLE_DB_HOST="${HANDLE_DB_POSTGRESQL_HOST}"
+        HANDLE_DB_PORT="${HANDLE_DB_POSTGRESQL_PORT}"
+    fi
     execute-sql-file.sh \
         --driver "${HANDLE_PERSISTENCE_TYPE}" \
         --host "${HANDLE_DB_HOST}" \
