9fe79fc07a6d0f7e3f607f1d200432c1d4f5686d
Jason Williams <54407063+jhujasonw@users.noreply.github.com>
2021-03-12 11:19:30 -0500

    Fixing the setup script to use the env var for DB_NAME (#32)
    * Fixing the setup script to use the env var for DB_NAME
    
    * Remove debug output
    
    * Fix typo, a little too backspace agressive.
    
    * Expose environment variables to php
    
    Allows nginx to pass system environment variables to php, so that
    settings, secrets, etc are available via $ENV, getenv(), etc
    
    * Pass DRUPAL_* env vars to php backends
    
    Adds an init script that creates nginx configuration that passes
    environment variables prefixed with DRUPAL_ as to fastcgi backends via
    statically defined fastcgi params.
    
    * Bump to Alpine 3.12.4
    
    Co-authored-by: Aaron Birkland <apb@jhu.edu>

--- drupal/rootfs/etc/cont-init.d/99-custom-setup.sh
+++ drupal/rootfs/etc/cont-init.d/99-custom-setup.sh
@@ -31,7 +31,7 @@ function execute_sql_file_local {
 function mysql_count_local {
     echo "SELECT COUNT(DISTINCT table_name)
 FROM information_schema.columns
-WHERE table_schema = 'drupal_default';" > /tmp/moo
+WHERE table_schema = '$(drupal_site_env "${site}" "DB_NAME")';" > /tmp/moo
     execute_sql_file_local /tmp/moo -- -N 2>/dev/null
 }
 
