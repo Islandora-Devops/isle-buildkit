01c1c7a3c883487d8ee6a2bdcce7c11ef10f07af
Elliot Metsger <emetsger@jhu.edu>
2021-04-13 07:46:27 -0600

    Correct the certificate data in the IdP and SP metadata (#39)
    * Correct the values of <ds:X509Certificate> in SP metadata.
    
    * Correct the values of <ds:X509Certificate> in IdP metadata.

--- idp/run-jetty.sh
+++ idp/run-jetty.sh
@@ -15,6 +15,11 @@ sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/shib-jetty-base/start.ini
 
 confd -onetime -backend file -file /run/secrets/saml_secrets -sync-only
 
+export _SP_SIGNING_CERT=$(yq eval '.saml-secrets.sp.signing-cert' /run/secrets/saml_secrets | sed -e '/^-----*/d'|tr -d '\n')
+export _IDP_SIGNING_CERT=$(yq eval '.saml-secrets.idp.signing-cert' /run/secrets/saml_secrets | sed -e '/^-----*/d'|tr -d '\n')
+export _IDP_ENCRYPTION_CERT=$(yq eval '.saml-secrets.idp.encryption-cert' /run/secrets/saml_secrets | sed -e '/^-----*/d'|tr -d '\n')
+export _IDP_BACKCHANNEL_CERT=$(yq eval '.saml-secrets.idp.backchannel-signing-cert' /run/secrets/saml_secrets | sed -e '/^-----*/d'|tr -d '\n')
+
 envsubst < /opt/shibboleth-idp/metadata/sp-metadata.xml > /tmp/sp-metadata.xml && cp /tmp/sp-metadata.xml /opt/shibboleth-idp/metadata/sp-metadata.xml
 envsubst < /opt/shibboleth-idp/metadata/idp-metadata.xml > /tmp/idp-metadata.xml && cp /tmp/idp-metadata.xml /opt/shibboleth-idp/metadata/idp-metadata.xml
 envsubst < /opt/shibboleth-idp/conf/attribute-filter.xml > /tmp/attribute-filter.xml && cp /tmp/attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml
