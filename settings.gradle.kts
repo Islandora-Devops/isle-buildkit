fcee86de684ea8c80f84b0abd4875012764469b5
Nigel Banks <nigel.g.banks@gmail.com>
2021-07-09 10:45:21 +0100

    Updated plugin/gradle to latest
    Can now perform build scans with `--scan` argument.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,5 @@
 rootProject.name = "isle-buildkit"
 
-// Include plugin for building Docker images.
-sourceControl {
-    gitRepository(uri("https://github.com/Islandora-Devops/isle-gradle-docker-plugin.git")) {
-        producesModule("ca.islandora:isle-gradle-docker-plugin")
-    }
-}
-
 // Include any folder that has a Dockerfile as a sub-project.
 rootProject.projectDir
     .walk()
