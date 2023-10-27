672b564767df99dbccefd8b26e0c4e329952236f
Gavin Morris <gavin@born-digital.com>
2023-10-27 11:40:50 -0400

    Draft 2 - max_packet_now has default env but still NOT dynamic. Confd env values ignored

--- mariadb/rootfs/etc/s6-overlay/scripts/set-max-packet.sh
+++ mariadb/rootfs/etc/s6-overlay/scripts/set-max-packet.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 set -e
 
-# Use what has been provided by the user or default to the derived values.
+# Use what has been provided by the user or default value of 256M
 cat <<EOF | /usr/local/bin/confd-import-environment.sh
     DB_MAX_ALLOWED_PACKET={{ getenv "DB_MAX_ALLOWED_PACKET" "${DB_MAX_ALLOWED_PACKET}" }}
 EOF
