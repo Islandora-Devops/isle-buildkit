c0d927ffcf5f544f252929f4058b768ebd151a2d
Gavin Morris <gavin@born-digital.com>
2023-10-30 16:22:40 -0400

    Remove three unneeded files

--- mariadb/rootfs/etc/s6-overlay/scripts/set-max-packet.sh
+++
@@ -1,8 +0,0 @@
-#!/command/with-contenv bash
-# shellcheck shell=bash
-set -e
-
-# Use what has been provided by the user or default value of 256M
-cat <<EOF | /usr/local/bin/confd-import-environment.sh
-    DB_MAX_ALLOWED_PACKET={{ getenv "DB_MAX_ALLOWED_PACKET" "${DB_MAX_ALLOWED_PACKET}" }}
-EOF
