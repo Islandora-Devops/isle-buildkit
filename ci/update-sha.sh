077ba468b86787a69f1463cf4ea6a4b62f5251b8
Joe Corall <jjc223@lehigh.edu>
2024-12-28 10:42:32 -0500

    Make sha update script more generic

--- ci/update-sha.sh
+++ ci/update-sha.sh
@@ -4,42 +4,34 @@ set -eou pipefail
 
 DEP=$1
 VERSION=$2
+URL=""
+ARG=""
+DOCKERFILE=""
 
-echo "Updating SHA for $DEP"
+echo "Updating SHA for $DEP@$VERSION"
 
 if [ "$DEP" = "apache-tomcat" ]; then
-  SHA=$(curl -s "https://downloads.apache.org/tomcat/tomcat-9/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz" \
-    | shasum -a 256 \
-    | awk '{print $1}')
-  echo "$SHA"
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's|^ARG TOMCAT_FILE_SHA256=".*"|ARG TOMCAT_FILE_SHA256="'"$SHA"'"|g' tomcat/Dockerfile
-  else
-    sed -i 's|^ARG TOMCAT_FILE_SHA256=".*"|ARG TOMCAT_FILE_SHA256="'"$SHA"'"|g' tomcat/Dockerfile
-  fi
+  URL="https://downloads.apache.org/tomcat/tomcat-9/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz"
+  ARG="TOMCAT_FILE_SHA256"
+  DOCKERFILE="tomcat/Dockerfile"
+elif [ "$DEP" = "apache-activemq" ]; then
+  URL="https://downloads.apache.org/activemq/$VERSION/apache-activemq-$VERSION-bin.tar.gz"
+  ARG="ACTIVEMQ_FILE_SHA256"
+  DOCKERFILE="activemq/Dockerfile"
+elif [ "$DEP" = "apache-solr" ]; then
+  URL="https://downloads.apache.org/solr/solr/$VERSION/solr-$VERSION.tgz"
+  ARG="SOLR_FILE_SHA256"
+  DOCKERFILE="solr/Dockerfile"
+else
+  echo "DEP not found"
+  exit 1
 fi
 
-if [ "$DEP" = "apache-activemq" ]; then
-  SHA=$(curl -s "https://downloads.apache.org/activemq/$VERSION/apache-activemq-$VERSION-bin.tar.gz" \
-    | shasum -a 256 \
-    | awk '{print $1}')
-  echo "$SHA"
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's|^ARG ACTIVEMQ_FILE_SHA256=".*"|ARG ACTIVEMQ_FILE_SHA256="'"$SHA"'"|g' activemq/Dockerfile
-  else
-    sed -i 's|^ARG ACTIVEMQ_FILE_SHA256=".*"|ARG ACTIVEMQ_FILE_SHA256="'"$SHA"'"|g' activemq/Dockerfile
-  fi
-fi
-
-if [ "$DEP" = "apache-solr" ]; then
-  SHA=$(curl -s "https://downloads.apache.org/solr/solr/$VERSION/solr-$VERSION.tgz" \
-    | shasum -a 256 \
-    | awk '{print $1}')
-  echo "$SHA"
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's|^ARG SOLR_FILE_SHA256=".*"|ARG SOLR_FILE_SHA256="'"$SHA"'"|g' solr/Dockerfile
-  else
-    sed -i 's|^ARG SOLR_FILE_SHA256=".*"|ARG SOLR_FILE_SHA256="'"$SHA"'"|g' solr/Dockerfile
-  fi
+SHA=$(curl -s "$URL" \
+  | shasum -a 256 \
+  | awk '{print $1}')
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' 's|^ARG '"$ARG"'=.*|ARG '"$ARG"'="'"$SHA"'"|g' "$DOCKERFILE"
+else
+  sed -i 's|^ARG '"$ARG"'=.*|ARG '"$ARG"'="'"$SHA"'"|g' "$DOCKERFILE"
 fi
