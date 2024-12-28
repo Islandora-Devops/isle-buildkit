dce178f63333224d279f1dce00bc28e46fe13404
Joe Corall <jjc223@lehigh.edu>
2024-12-28 11:48:57 -0500

    Use advanced capture for github releases

--- ci/update-sha.sh
+++ ci/update-sha.sh
@@ -14,22 +14,52 @@ if [ "$DEP" = "apache-tomcat" ]; then
   URL="https://downloads.apache.org/tomcat/tomcat-9/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz"
   ARG="TOMCAT_FILE_SHA256"
   DOCKERFILE="tomcat/Dockerfile"
+
 elif [ "$DEP" = "apache-activemq" ]; then
   URL="https://downloads.apache.org/activemq/$VERSION/apache-activemq-$VERSION-bin.tar.gz"
   ARG="ACTIVEMQ_FILE_SHA256"
   DOCKERFILE="activemq/Dockerfile"
+
 elif [ "$DEP" = "apache-solr" ]; then
   URL="https://downloads.apache.org/solr/solr/$VERSION/solr-$VERSION.tgz"
   ARG="SOLR_FILE_SHA256"
   DOCKERFILE="solr/Dockerfile"
+
 elif [ "$DEP" = "custom-composer" ]; then
   URL="https://getcomposer.org/download/${VERSION}/composer.phar"
   ARG="COMPOSER_SHA256"
   DOCKERFILE="nginx/Dockerfile"
+
 elif [ "$DEP" = "solr-ocrhighlighting" ]; then
   URL=https://github.com/dbmdz/solr-ocrhighlighting/releases/download/${VERSION}/solr-ocrhighlighting-${VERSION}.jar
   ARG="OCRHIGHLIGHT_FILE_SHA256"
   DOCKERFILE="solr/Dockerfile"
+
+elif [ "$DEP" = "alpine-pkg-glibc" ]; then
+  URL="https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${VERSION}/glibc-${VERSION}.apk"
+  ARG="GLIBC_SHA256"
+  DOCKERFILE="code-server/Dockerfile"
+
+elif [ "$DEP" = "fcrepo" ]; then
+  URL="https://github.com/fcrepo/fcrepo/releases/download/fcrepo-${VERSION}/fcrepo-webapp-${VERSION}.war"
+  ARG="FCREPO_SHA256"
+  DOCKERFILE="fcrepo6/Dockerfile"
+
+elif [ "$DEP" = "islandora-syn" ]; then
+  URL="https://github.com/Islandora/Syn/releases/download/v${VERSION}/islandora-syn-${VERSION}-all.jar"
+  ARG="SYN_SHA256"
+  DOCKERFILE="fcrepo6/Dockerfile"
+
+elif [ "$DEP" = "fcrepo-import-export" ]; then
+  URL="https://github.com/fcrepo-exts/fcrepo-import-export/releases/download/fcrepo-import-export-${VERSION}/fcrepo-import-export-${VERSION}.jar"
+  ARG="IMPORT_EXPORT_SHA256"
+  DOCKERFILE="fcrepo6/Dockerfile"
+
+elif [ "$DEP" = "fcrepo-upgrade-utils" ]; then
+  URL="https://github.com/fcrepo-exts/fcrepo-upgrade-utils/releases/download/fcrepo-upgrade-utils-${VERSION}/fcrepo-upgrade-utils-${UPGRADE_UTILS_VERSION}.jar"
+  ARG="UPGRADE_UTILS_SHA256"
+  DOCKERFILE="fcrepo6/Dockerfile"
+
 else
   echo "DEP not found"
   exit 0
