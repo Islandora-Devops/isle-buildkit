6767a9862c803da6d9e541092da759845660ef6c
Nigel Banks <nigel.g.banks@gmail.com>
2025-06-30 16:58:07 +0200

    Fixed failing test for Transkribus.

--- transkribus/tests/ServiceStartsWithDefaults/test.sh
+++ transkribus/tests/ServiceStartsWithDefaults/test.sh
@@ -5,7 +5,11 @@
 source /usr/local/share/isle/utilities.sh
 
 # Wait for service to start.
-wait_20x http://localhost:5000/
+# We don't check if it is functioning as it requires a Transkribus account.
+echo "Waiting for reponse on http://localhost:5000/"
+while status=$(curl -s -w "%{http_code}" http://localhost:5000/ 2>/dev/null || echo "000") && [ "$status" != "400" ]; do
+  sleep 5
+done
 
 # Service must start for us to get to this point.
 exit 0
