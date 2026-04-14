3dd4a78520a051acedd6b5fa647b4c906200b8b1
Joe Corall <joe@libops.io>
2026-04-14 08:57:39 -0400

    fixup test

--- images/solr/tests/ServiceStartsWithCustomModules/test.sh
+++ images/solr/tests/ServiceStartsWithCustomModules/test.sh
@@ -9,10 +9,13 @@ source /usr/local/share/isle/utilities.sh
 # Wait for service to start.
 wait_20x http://localhost:8983/solr
 
-# Solr should translate SOLR_MODULES into a JVM system property for the live process.
-process="$(pgrep -u solr -af -- "-Dsolr.modules=")"
+# SOLR_MODULES should be present in the environment inherited by the live Solr process.
+pid="$(pgrep -u solr -n java)"
+process_env="$(s6-setuidgid solr sh -c "tr '\0' '\n' < /proc/${pid}/environ")"
+
+[[ "${process_env}" == *"SOLR_MODULES="* ]]
 
 IFS=',' read -r -a modules <<< "${SOLR_MODULES}"
 for module in "${modules[@]}"; do
-  [[ "${process}" == *"${module}"* ]]
+  [[ "${process_env}" == *"${module}"* ]]
 done
