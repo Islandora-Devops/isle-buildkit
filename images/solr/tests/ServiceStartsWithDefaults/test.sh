5ea71d60521c056022130054bdaa7c2a729c9a23
Joe Corall <jjc223@lehigh.edu>
2026-04-15 13:28:00 -0400

    solr v10 readiness: add SOLR_MODULES (#431)
    * define SOLR_MODULES
    
    * Do not attempt to create the core if it already exists
    
    * bump python
    
    * Keep solr version up to date in utilities.sh
    
    * fixup test
    
    * bump ca-certs
    
    * solr 10 readiness
    
    * Create symlink for ocrhighlighting.jar
    
    Add symbolic link for ocrhighlighting.jar in Dockerfile

--- images/solr/tests/ServiceStartsWithDefaults/test.sh
+++ images/solr/tests/ServiceStartsWithDefaults/test.sh
@@ -7,5 +7,9 @@ source /usr/local/share/isle/utilities.sh
 # Wait for service to start.
 wait_20x http://localhost:8983/solr
 
+# The OCR highlighting plugin should be installed as a plain Solr lib so it
+# remains available after Solr 10 removes <lib .../> loading.
+find /opt/solr/lib -maxdepth 1 -type f -name 'solr-ocrhighlighting-*.jar' | grep -q .
+
 # Service must start for us to get to this point.
 exit 0
